5af6eefed57c46f22fdea436e182b5dc5b4ac585
77f315 <77f315@gmail.com>
2026-02-05 22:26:37 +0200

    lines

---
+++ road.js
@@ -0,0 +1,34 @@
+class Road{
+    constructor(x,width, laneCount=3){
+        this.x=x;
+        this.width=width;
+        this.laneCount=laneCount;
+
+        this.left = x-width/2;
+        this.right = x+width/2;
+        
+        const inf =1000000;
+        this.top =-inf;
+        this.bottom =inf;
+    }
+
+    draw(ctx){
+        ctx.lineWidth=5;
+        ctx.strokeStyle="white";
+
+        for (let i=0; i<=this.laneCount;i++){
+            const x =lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            )
+            
+        ctx.beginPath();
+        ctx.moveTo(x,this.top);
+        ctx.lineTo(x,this.bottom);
+        ctx.stroke();
+        }
+
+    }
+}
+
